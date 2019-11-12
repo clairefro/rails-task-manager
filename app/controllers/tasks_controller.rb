e406b2cebd0ff4b813e03daa25840e6ccd25ff2c
claire froelich <claire.froelich@gmail.com>
2019-11-12 11:59:13 -0500

    show page working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,15 @@ class TasksController < ApplicationController
   # READ
   def index
     @tasks = Task.all
+
   end
 
   def show
+    set_task
   end
 
-
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
